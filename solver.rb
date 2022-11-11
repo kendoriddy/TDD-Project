f68b4cc0705a1680d54cd46d594f2dbc0dff2473
Anas <anas.chaabane98@gmail.com>
2022-11-11 13:07:55 +0100

    solver-methods

--- solver.rb
+++ solver.rb
@@ -1,7 +1,27 @@
 class Solver
-  def factorial(num); end
+  def factorial(num)
+    raise ArgumentError, 'negative integers are not allowed' if num.negative?
 
-  def reverse(word); end
+    if [0, 1].include?(num)
+      1
+    else
+      num * factorial(num - 1)
+    end
+  end
 
-  def fizz_buzz; end
+  def reverse(word)
+    word.reverse!
+  end
+
+  def fizz_buzz(num)
+    if num.modulo(3).zero? && num.modulo(5).zero?
+      'fizzbuzz'
+    elsif num.modulo(3).zero?
+      'fizz'
+    elsif num.modulo(5).zero?
+      'buzz'
+    else
+      num.to_s
+    end
+  end
 end
